305dce141fffee3288bfe8e9ed5a9a092a68d3af
IshaSal <you@example.com>
2022-11-03 23:58:15 -0400

    Part 2

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,11 +1,15 @@
+//array of words
 let words = [];
 
+//search function in personal dictionary
   function search (words){
 return words.includes(words);
   }
 
+  //function to add word
   function addWord (words){
 words.push(push)
   }
 
+  //function to see the word needed
   export { search, addWord }
